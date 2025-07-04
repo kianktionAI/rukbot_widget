ea53c85dd0938058e7c33a46d34c3f4772e1dfca
Sean Scoffield <seanscoffield@Seans-MacBook-Air.local>
2025-07-04 21:48:30 +1000

    Fix icon path

--- public/rukbot-widget.js
+++ public/rukbot-widget.js
@@ -24,8 +24,7 @@
     // Create button
     const btn = document.createElement('div');
     btn.id = 'rukbot-widget-btn';
-    btn.innerHTML = '<img src="https://rukbot-widget.onrender.com/rukbot_icon.png" alt="RUKBOT" style="width:40px;height:40px;" />';  
-    // Create chat window
+    btn.innerHTML = '<img src="https://rukbot-widget.onrender.com/rukbot_icon.png" alt="RUKBOT" style="width:40px;height:40px;" />';      // Create chat wind
     const chat = document.createElement('div');
     chat.id = 'rukbot-widget-chat';
     chat.innerHTML = `
